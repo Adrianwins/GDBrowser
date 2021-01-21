04c6d1309dd7c197cf97c54bd09a4a6d2977a714
Adrianwins <77806253+Adrianwins@users.noreply.github.com>
2021-01-21 14:51:09 -0500

    Update postProfileComment.js

--- api/post/postProfileComment.js
+++ api/post/postProfileComment.js
@@ -13,7 +13,6 @@ module.exports = async (app, req, res) => {
   let params = { cType: '1' }
 
   params.comment = Buffer.from(req.body.comment.slice(0, 190) + (req.body.color ? "☆" : "")).toString('base64').replace(/\//g, '_').replace(/\+/g, "-")
-  params.gjp = app.xor.encrypt(req.body.password, 37526)
   params.accountID = req.body.accountID.toString()
   params.userName = req.body.username
 
